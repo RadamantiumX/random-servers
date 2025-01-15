6240c1c8dbe47f7aac3167503fe78e8bf94a2a19
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-01-15 18:50:31 -0300

    latest notes

--- atomic/ATOMICS.md
+++ atomic/ATOMICS.md
@@ -9,3 +9,9 @@ Muchas veces, se pueden compartir diferentes tipos de datos entre los THREADS, p
 Estos representan un *GENERIC RAW BINARY DATA BUFFER*, lo que significa, que admiten datos binarios largos (0,1), basicamente cosas de bajo nivel.
 
 Cuando intentamos pasar estos ARRAY BUFFER de una THREAD a otra, este se separará de esa THREAD de origen, es decir, ya no tendrá acceso a el.
+Hay mejor forma de trabajar con eso, y se llama *SharedArrayBuffer*. Lo que hace, es que se pueden compartir los BUFFER DE MEMORIA entre las THREADS.
+
+## TYPED ARRAYS
+
+Se utilizan en CANVAS, WEBGL. Podemos crearlos pero no modificarlos, en su lugar tiene un metodo, llamado *DataView()*, el cual puede ser obtenido, seteado y modificado.
+Pero eso es un incoveniente, y es ahi donde entra ATOMIC, que nos permite modificar el valor del BUFFER
--- atomic/index.js
+++ atomic/index.js
@@ -1,7 +1,9 @@
 import { Worker, isMainThread, workerData, threadId } from "node:worker_threads"
 
 if(isMainThread){ // Verificamos si estamos en la THREAD principal
-    const buffer = new SharedArrayBuffer(1) // Creamos el buffer
+
+    // Se comparten el MEMORY BUFFER entre varias THREADS
+    const buffer = new SharedArrayBuffer(1) // Creamos el buffer, le asignamos 1 BYTE a ese ARRAYBUFFER
     // Lo pasamos a los WORKERS creados
     // Creamos los WORKERS en el mismo archivo
     // Lo ideal es separarlos
@@ -11,8 +13,10 @@ if(isMainThread){ // Verificamos si estamos en la THREAD principal
     const typedArray = new Int8Array(workerData) // Creamos un *Typed array* con workerData's (No es un ARRAY, no puede utilizar sus metodos)
     console.log("typedArray", typedArray)
 
-    typedArray[0] = threadId
-    // const value = Atomics.store(typedArray, 0, threadId)
+    // typedArray[0] = threadId // NO! puede generar BUGS
+
+    // Guardamos el valor en ATOMIC, le asignamos el 0 y el indice.
+    const value = Atomics.store(typedArray, 0, threadId)
 
     console.dir({ threadId, value: typedArray[0] })
 }
