fa780ecfbf102f5d17aebda9316d8c094e16beec
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-01-14 23:40:03 -0300

    last changes

--- streams/index.js
+++ streams/index.js
@@ -1,10 +1,23 @@
 import fs from 'fs'
 
 /* Readable Streams */
-
-const readable = fs.createReadStream("./my-file.tsxt", { highWaterMark: 20 })
+// Archivo de texto = 1000 Bytes --> Chunk = 20 Byte en cada parte
+const readable = fs.createReadStream("./my-file.txt", { highWaterMark: 20 }) // El "highWaterMark" es un limite para el BUFFER
 
 // Va a escuchar el evento "data", va a recibir un "chunk"
-readable.on("data", (chunk) => { // El "chunk" es un representacion BINARIA
+/*readable.on("data", (chunk) => { // El "chunk" es un representacion BINARIA
     console.log("New chunk:", chunk.toString()) // Se mostrará por consola un nuevo fragmento convertido de nuevo a cadena.
-})
+})*/
+
+/*(async ()=> {
+    for await (const chunk of readable){
+        console.log("New chunk:", chunk.toString())
+    }
+})()*/
+
+
+/* Writable Streams */
+
+const writable = fs.createWriteStream("my-new-file.txt")
+writable.write("Hello, ")
+writable.end("world!")
