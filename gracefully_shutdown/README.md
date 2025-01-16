7338f789ff07db984f77bfe9f524cc5928cd8e14
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-01-16 09:35:58 -0300

    last notes

--- gracefully_shutdown/README.md
+++ gracefully_shutdown/README.md
@@ -2,3 +2,11 @@
 
 Un servidor que se cierra, por alguna razón, puede traer problemas, sobre todo en las transacciones con las BD, que pueden realizarce o no. Es por eso que debemos cerrar todas las conexiones, antes de cerrar el servidor, es ahi donde entra el *GRACEFULLY SHUTDOWN*.
 Esta acción realiza una limpieza antes de cerrar el servidor.
+
+*Los CHILD PROCESS tambien pueden detenerse de esta forma.*
+
+
+## Apagado con elegancia CONTENEDOR DOCKER
+
+El contenedor DOCKER enviará el SIGTERM antes que lo detengamos, para detener los servicios. Si no lo logramos detener, de esta forma elegante, despues de 10 segundos lo hará de forma forzada (localmente podemos aumentar ese tiempo).
+
--- gracefully_shutdown/index.js
+++ gracefully_shutdown/index.js
@@ -16,10 +16,10 @@ const server = app.listen(3000, ()=>{
 process.on("SIGTERM", () => {
     console.log("SIGTERM signal received.")
     server.close(()=>{
-       console.log("Closed out remaining connections")
-       // closeDBConnection()
-       // closeWSConnection()
-       // child.kill('SIGINT')
+      console.log("Closed out remaining connections")
+       closeDBConnection()
+       closeWSConnection()
+       child.kill('SIGINT') // Detenemos los CHILD PROCESS
 
        // Additional cleanup tasks go here
 
@@ -31,11 +31,32 @@ process.on("SIGINT", () => {
   console.log("SIGINT signal received")
   server.close(()=>{
     console.log("Closed out remaining connections")
-       // closeDBConnection()
-       // closeWSConnection()
-       // child.kill('SIGINT')
+       closeDBConnection()
+       closeWSConnection()
+       child.kill('SIGINT') // Detenemos los CHILD PROCESS
 
        // Additional cleanup tasks go here
        process.exit(0)
   })
 })
+
+// Cerramos MySQL
+function closeDBConnection(){
+    connection.end((err)=>{
+        if(err) {
+            console.error("Error closing MySQL connection", err)
+            process.exit(1)
+        } else {
+            console.log("MySQL connection closed.")
+            process.exit(0)
+        }
+    })
+}
+
+// Cerramos los WEBSOCKETS
+function closeWSConnection(){
+    ws.close(1000, "Process terminated") // 100 is the normal closure code
+}
+
+// Si tuvieramos una conexion al servicio de RabbitMQ, tambien lo podemos cerrar 
+// RabbitMQ es un servicio de mensajeria en tiempo real, para chats
