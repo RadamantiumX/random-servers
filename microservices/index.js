639ecf4a92cdaeec375d2bddf05e893094d96f18
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-01-24 18:50:51 -0300

    testing user service

--- microservices/index.js
+++ microservices/index.js
@@ -1,24 +1,52 @@
-import { ServiceBroker } from "moleculer";
+import UserService from './services/user.service.js';
+// import { ServiceBroker } from "moleculer";
+// const broker = new ServiceBroker()
 
+// // Inicializamos el Servicio
+// broker.createService({
+//     name: 'greeter',
+//     actions: {
+//         sayHello(ctx){ // Recibe un contexto (ctx) de parametro
+//          return `Hello ${ctx.params.name}` // Retorna un saludo con el nombre recibido
+//         }
+//     }
+// })
 
-const broker = new ServiceBroker()
+// // Creamos una función asincrona para iniciar el broker y llamar al servicio greeter
+// async function startApp(){
+//     await broker.start()
+//     const res = await broker.call('greeter.sayHello', {name: 'John'}) // Llama al servicio greeter y le pasa el nombre John
+//     console.log(res) // Imprime el resultado
+//     broker.stop() // Detiene el broker
+// }
 
-// Inicializamos el Servicio
-broker.createService({
-    name: 'greeter',
-    actions: {
-        sayHello(ctx){ // Recibe un contexto (ctx) de parametro
-         return `Hello ${ctx.params.name}` // Retorna un saludo con el nombre recibido
-        }
+// startApp()
+
+async function startApp() {
+    // Inciando Servicios
+    await UserService.start()
+
+    try{
+        // Simulate User creation
+        // Llamamos al servicio
+        // Creamos un Usuario
+        const newUser = await UserService.call('users.createUser', 
+            {
+                username: 'John', 
+                email: 'john@mail.com'
+
+            })
+            console.log('New user created:', newUser)
+
+        // Obtenemos los Usuarios    
+        const users = await UserService.call('users.getUsers')
+        console.log('Users:', users)
+    }catch(err){
+         console.error('Error:', err)
+    } finally {
+        // Detenemos el Servicio sin importar que suceda
+        await UserService.stop()
     }
-})
-
-// Creamos una función asincrona para iniciar el broker y llamar al servicio greeter
-async function startApp(){
-    await broker.start()
-    const res = await broker.call('greeter.sayHello', {name: 'John'}) // Llama al servicio greeter y le pasa el nombre John
-    console.log(res) // Imprime el resultado
-    broker.stop() // Detiene el broker
 }
 
 startApp()
