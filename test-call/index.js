371c23cf564b77ee9d28229e3525cad53a92b4d9
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-02-01 16:35:51 -0300

    somo extra change

--- test-call/index.js
+++ test-call/index.js
@@ -1,20 +1,23 @@
-function call() {
-    fetch(
+async function call() {
+    const data = await fetch(
         "https://api.igdb.com/v4/games",
         { method: 'POST',
           headers: {
             'Accept': 'application/json',
-            'Client-ID': 'Client ID',
-            'Authorization': 'Bearer access_token',
+            'Client-ID': 'lc2l8nn238z8icmqdanuzp4q7wy8hc',
+            'Authorization': 'Bearer 6leiw22e28up5fbwelvl2uuxhmx1fc',
           },
           body: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,collections,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
       })
         .then(response => {
-            console.log(response.json());
+            const res = response.json();
+            console.log(res);
         })
         .catch(err => {
             console.error(err);
         });
+
+        console.log(data)
 }
 
 call()
